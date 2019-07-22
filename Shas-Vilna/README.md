e5eb9074a7dcfd2e96cbd9a8f444111d1dc81421
Shaye Ulman <47426218+shayeUlman@users.noreply.github.com>
2019-07-22 23:31:56 +0300

    Fixed the "dibbur grouping" issue

---
+++ Shas-Vilna/README.md
@@ -0,0 +1,7 @@
+# Shas-Vilna
+An attempt to recreate the Vilna edition of the Talmud Bavli on the web.
+
+##Screenshot:
+
+![alt text](./assets/img/Screenshot.png)
+
---
+++ Shas-Vilna/yiddishe-kop.js
@@ -0,0 +1,119 @@
+jQuery(document).ready(function ($) {
+
+  $('.document .wrapper').html(function () {
+    return $(this).html().replace(/\<br\>/gi, '<i></i>');
+  })
+
+  $('.rashi, .tosafos').each(function () {
+
+    let html = $(this).html();
+    html = html.replace(/[\n\r]\s*/g, '') // remove returns/whitespace
+    html = html.replace(/(\(.+?\))/g, '<small>$1</small>') // make mekoros small
+
+    if ($(this).hasClass('rashi')) {
+
+      // doing some magic with GREP 🤩
+      // for browser that don't yet support lookbehind
+      html = html.replace(/(:\s|\<span.+?md.+?\>)(.+?\.)(.+?(?=:))/g, (match, g1, dibbur, body) => {
+        dibbur = dibbur.replace(/(\<\/span\>\<span.+?(sm|md|lg).+?\>)/g, '</span>$1<span class="dibbur">')
+        body = body.replace(/(\<\/span\>\<span.+?(sm|md|lg).+?\>)/g, '</span>$1<span class="rashi-body">')
+        return `${g1}<span class="dibbur">${dibbur}</span><span class="rashi-body">${body}</span>`
+      })
+      html = html.replace(/(\^)([א-ת.]+)/g, (match, g1, firstWord) => { // creates BIG WORD - only if beginning of line
+        return `${g1.replace('^', '')}<span class="chalon">${firstWord}</span>`
+      })
+    } else { // tosfot
+      html = html.replace(/\@1(.+?)\@2(.+?)\@3/g, (match, dibbur, body) => { // creates dibbur & body - from @1 @2 @3
+        dibbur = dibbur.replace(/(\<\/span\>\<span.+?(sm|md|lg).+?\>)/g, '</span>$1<span class="dibbur">')
+        body = body.replace(/(\<\/span\>\<span.+?(sm|md|lg).+?\>)/g, '</span>$1<span class="tosfot-body">')
+        return `<span class="dibbur">${dibbur}</span><span class="tosfot-body">${body}</span>`
+      })
+      html = html.replace(/(^\<span.+?md.+?\>|\<\/i\>\<span.+?dibbur.+?\>)([א-ת.]+)/g, (match, g1, firstWord) => { // creates BIG WORD - only if beginning of line
+        return `${g1}<span class="chalon">${firstWord}</span>`
+      })
+    }
+
+    $(this).html(html)
+
+    createChalonSpacer($(this).hasClass('rashi') ? '.rashi' : '.tosafos')
+
+    // create inline dibbur in tosfos
+    $('.tosafos .dibbur').each(function () {
+      if (!$(this).find(">:first-child").hasClass('chalon')) {
+        let html = $(this).html()
+        html = html.replace(/^.*?([א-ת״׳.]+)/, '<span class="tos-not-chalon">$1</span>')
+        $(this).html(html)
+      }
+    })
+  });
+
+
+  // Grouping together every dibbur with special class
+  let counter = 0;  // Global
+  $('.wrapper span.rashi-body, .wrapper span.tosfot-body, .wrapper span.dibbur').html(function () {
+    let thisClass = $(this).attr('class');
+    if ($(this).next()[0]) return;
+    let next = $(this).parent().next().find(">:first-child")
+    if (next.hasClass(thisClass)) {
+      let connectorClassName = `connector-${counter++}`;
+      $(this).addClass(connectorClassName);
+      next.addClass(connectorClassName);
+      if (!next.next()[0]) { // should really find a recursive solution [maybe you can help, Shaye 😊 ?]
+        let next2 = next.parent().next().find(">:first-child")
+        if (next2.hasClass(thisClass)) {
+          next2.addClass(connectorClassName);
+        }
+      }
+    }
+  });
+
+
+  // adss hover class to each group
+  $('.wrapper span.rashi-body, .wrapper span.tosfot-body, .wrapper span.dibbur').hover(function () {
+    if ($(this).attr("class").includes('connector')) {
+      const groupClassName = $(this).attr("class").split(' ').filter(c => c.includes('connector'));
+        $('.' + groupClassName).toggleClass('hover');
+    }
+  });
+
+
+  // creates biur
+  $('.dibbur').attr({
+    'data-container': 'body',
+    'data-toggle': "popover",
+    'data-placement': "left",
+    'title': function () {
+      return $(this).text()
+    },
+    'data-content': function () {
+      return $(this).next().text()
+    }
+  })
+
+  // creates popup for mekoros
+  $('.wrapper small').attr({
+    'data-toggle': 'modal',
+    'data-target': "#exampleModalCenter",
+  })
+
+  $('[data-toggle="popover"]').popover({
+    trigger: 'hover',
+    animation: true
+  })
+
+  function createChalonSpacer(selector) {
+
+    // spacer under CHALON
+    let chalonWidths = []
+    $(selector + ' .chalon').each(function () {
+      chalonWidths.push($(this)[0].offsetWidth)
+    })
+
+    let i = 0;
+    html = $(selector).html();
+    html = html.replace(/(chalon.+?\<\/i\>)(.)/g, (match, g1, nextLineFirstChar) => { // creates CHALON spacer
+      return `${g1}<span class='chalon-spacer' style='width: ${chalonWidths[i++]}px;'></span>${nextLineFirstChar}`
+    })
+    $(selector).html(html)
+  }
+})
